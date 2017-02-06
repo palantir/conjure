adcc99265d930ad4f0925a6d93fdb3d223f71d45
Robert Fink <rfink@palantir.com>
2017-02-06 16:43:20 -0500

    Remove unnecessary SSL trust code in tests (#196)

--- conjure-java-gradle-plugin/src/test/groovy/com/palantir/conjure/ConjureJavaPluginTest.groovy
+++ conjure-java-gradle-plugin/src/test/groovy/com/palantir/conjure/ConjureJavaPluginTest.groovy
@@ -32,27 +32,6 @@ class ConjureJavaPluginTest extends GradleTestSpec {
             generatedCompile "javax.ws.rs:javax.ws.rs-api:2.0.1"
         }
 
-        // Work-around to trust all HTTPS hosts.
-        import javax.net.ssl.HostnameVerifier
-        import javax.net.ssl.HttpsURLConnection
-        import javax.net.ssl.SSLContext
-        import javax.net.ssl.TrustManager
-        import javax.net.ssl.X509TrustManager
-        def nullTrustManager = [
-                checkClientTrusted: { chain, authType ->  },
-                checkServerTrusted: { chain, authType ->  },
-                getAcceptedIssuers: { null }
-        ]
-
-        def nullHostnameVerifier = [
-                verify: { hostname, session -> true }
-        ]
-
-        SSLContext sc = SSLContext.getInstance("SSL")
-        sc.init(null, [nullTrustManager as X509TrustManager] as TrustManager[], null)
-        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory())
-        HttpsURLConnection.setDefaultHostnameVerifier(nullHostnameVerifier as HostnameVerifier)
-
         afterEvaluate {
             sourceSets.generated.runtimeClasspath.each {
                 if (it.toString().contains("build/resources/generated")) {
