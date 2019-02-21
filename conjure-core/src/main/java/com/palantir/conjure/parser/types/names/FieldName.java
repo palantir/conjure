82282db55d7ee62533eb3bdab2ad1953e203a191
Adam <aborochoff@gmail.com>
2019-02-21 08:09:02 -0500

    [fix] Get rid of the patterns in FieldName to allow single character field names (#234)
    <!-- PR title should start with '[fix]', '[improvement]' or '[break]' if this PR would cause a patch, minor or major SemVer bump. Omit the prefix if this PR doesn't warrant a standalone release. -->
    
    ## Before this PR
    <!-- Describe the problem you encountered with the current state of the world (or link to an issue) and why it's important to fix now. -->
    
    There were 2 validations for field names.  A previous PR attempted to allow single character field names, but there were 2 places validations happened.
    
    ## After this PR
    <!-- Describe at a high-level why this approach is better. -->
    
    Allow single character field names and prevent the issue from happening again by having only 1 source of truth for the validation patterns.
    
    <!-- Reference any existing GitHub issues, e.g. 'fixes #000' or 'relevant to #000' -->

--- conjure-core/src/main/java/com/palantir/conjure/parser/types/names/FieldName.java
+++ conjure-core/src/main/java/com/palantir/conjure/parser/types/names/FieldName.java
@@ -18,12 +18,10 @@ package com.palantir.conjure.parser.types.names;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
-import com.google.common.base.CaseFormat;
 import com.google.common.base.Preconditions;
+import com.palantir.conjure.CaseConverter;
 import com.palantir.conjure.defs.ConjureImmutablesStyle;
 import com.palantir.conjure.parser.types.complex.ObjectTypeDefinition;
-import java.util.Arrays;
-import java.util.regex.Pattern;
 import org.apache.commons.lang3.StringUtils;
 import org.immutables.value.Value;
 import org.slf4j.Logger;
@@ -38,50 +36,25 @@ public abstract class FieldName {
 
     private static final Logger log = LoggerFactory.getLogger(FieldName.class);
 
-    private static final Pattern CAMEL_CASE_PATTERN =
-            Pattern.compile("^[a-z]([A-Z]{1,2}[a-z0-9]|[a-z0-9])*[A-Z]?$");
-    private static final Pattern KEBAB_CASE_PATTERN =
-            Pattern.compile("^[a-z]((-[a-z]){1,2}[a-z0-9]|[a-z0-9])*(-[a-z])?$");
-    private static final Pattern SNAKE_CASE_PATTERN =
-            Pattern.compile("^[a-z]((_[a-z]){1,2}[a-z0-9]|[a-z0-9])*(_[a-z])?$");
-
-    public enum Case {
-        LOWER_CAMEL_CASE(CAMEL_CASE_PATTERN, CaseFormat.LOWER_CAMEL),
-        KEBAB_CASE(KEBAB_CASE_PATTERN, CaseFormat.LOWER_HYPHEN),
-        SNAKE_CASE(SNAKE_CASE_PATTERN, CaseFormat.LOWER_UNDERSCORE);
-
-        private final Pattern pattern;
-        private final CaseFormat format;
-
-        Case(Pattern pattern, CaseFormat format) {
-            this.pattern = pattern;
-            this.format = format;
-        }
-
-        @Override
-        public String toString() {
-            return name() + "[" + pattern + "]";
-        }
-
-        public String convertTo(FieldName fieldName, Case targetCase) {
-            return format.to(targetCase.format, fieldName.name());
-        }
-    }
-
     @JsonValue
     public abstract String name();
 
     @Value.Check
     @SuppressWarnings("Slf4jLogsafeArgs")
     protected final void check() {
-        Preconditions.checkArgument(
-                Case.LOWER_CAMEL_CASE.pattern.matcher(name()).matches()
-                        || Case.KEBAB_CASE.pattern.matcher(name()).matches()
-                        || Case.SNAKE_CASE.pattern.matcher(name()).matches(),
-                "FieldName \"%s\" must follow one of the following patterns: %s",
-                name(), Arrays.toString(Case.values()));
 
-        if (!Case.LOWER_CAMEL_CASE.pattern.matcher(name()).matches()) {
+        CaseConverter.Case lowerCamelCase = CaseConverter.Case.LOWER_CAMEL_CASE;
+        CaseConverter.Case kebabCase = CaseConverter.Case.KEBAB_CASE;
+        CaseConverter.Case snakeCase = CaseConverter.Case.SNAKE_CASE;
+
+        Preconditions.checkArgument(
+                lowerCamelCase.getPattern().matcher(name()).matches()
+                        || kebabCase.getPattern().matcher(name()).matches()
+                        || snakeCase.getPattern().matcher(name()).matches(),
+                "FieldName \"%s\" must follow one of the following patterns: %s %s %s",
+                name(), lowerCamelCase, kebabCase, snakeCase);
+
+        if (!lowerCamelCase.getPattern().matcher(name()).matches()) {
             log.warn("{} should be specified in lowerCamelCase. kebab-case and snake_case are supported for "
                     + "legacy endpoints only: {}", FieldName.class, name());
         }
@@ -89,9 +62,9 @@ public abstract class FieldName {
 
     /** Returns the case of this field name. */
     @Value.Lazy
-    protected Case nameCase() {
-        for (Case nameCase : Case.values()) {
-            if (nameCase.pattern.matcher(name()).matches()) {
+    protected CaseConverter.Case nameCase() {
+        for (CaseConverter.Case nameCase : CaseConverter.Case.values()) {
+            if (nameCase.getPattern().matcher(name()).matches()) {
                 return nameCase;
             }
         }
@@ -104,8 +77,8 @@ public abstract class FieldName {
     }
 
     /** Converts this {@link FieldName} to a {@link FieldName} with the given case. */
-    public final FieldName toCase(Case targetCase) {
-        return FieldName.of(nameCase().convertTo(this, targetCase));
+    public final FieldName toCase(CaseConverter.Case targetCase) {
+        return FieldName.of(nameCase().convertTo(this.name(), targetCase));
     }
 
     /**
