f3034cc8768188f493265e8137d387f23d2f3d5d
Luke Tomlin <ltomlin@palantir.com>
2021-03-16 13:23:44 +0000

    Fix tests

--- conjure-core/src/test/java/com/palantir/conjure/parser/types/TypeParserTests.java
+++ conjure-core/src/test/java/com/palantir/conjure/parser/types/TypeParserTests.java
@@ -178,8 +178,8 @@ public final class TypeParserTests {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(
                         "TypeNames must be a primitive type [datetime, boolean, string, double, bearertoken, binary,"
-                                + " safelong, integer, rid, any, uuid] or match pattern ^[A-Z][a-z0-9]+([A-Z][a-z0-9]+)*$:"
-                                + " %s",
+                                + " safelong, integer, rid, any, uuid] or match pattern"
+                                + " ^[A-Z][a-z0-9]+([A-Z][a-z0-9]+)*$: %s",
                         invalid);
     }
 }
