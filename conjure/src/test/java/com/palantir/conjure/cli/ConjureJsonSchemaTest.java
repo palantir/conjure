9a44896c72598bd0d7e6e42248742c7b1ff5d037
Nicholas Gates <gatesn@users.noreply.github.com>
2022-09-28 16:30:37 +0100

    Create Conjure JSON schema (#1252)

---
+++ conjure/src/test/java/com/palantir/conjure/cli/ConjureJsonSchemaTest.java
@@ -0,0 +1,135 @@
+/*
+ * (c) Copyright 2022 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.conjure.cli;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.core.JsonLocation;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.google.common.base.Splitter;
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.spi.json.JacksonJsonNodeJsonProvider;
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.SpecVersion.VersionFlag;
+import com.networknt.schema.ValidationMessage;
+import com.networknt.schema.ValidatorTypeCode;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/**
+ * Test each of the definition files against the JSON schema definition.
+ *
+ * For any definition files that are intended to fail, the failing line should be annotated with a comment of the form:
+ *
+ *   # SCHEMA(foo, bar): some message explaining why this is an intended failure
+ *
+ * Where 'foo' and 'bar' are error codes from {@link ValidatorTypeCode}.
+ */
+public class ConjureJsonSchemaTest {
+
+    private static final Pattern failurePattern = Pattern.compile("\\s*# SCHEMA\\(([\\w\\s,]+)\\):.*");
+    private static final JsonSchema schema;
+
+    static {
+        try {
+            schema = JsonSchemaFactory.getInstance(VersionFlag.V7)
+                    .getSchema(Files.newInputStream(Paths.get("../conjure.schema.json")));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("definitionFiles")
+    void testJsonSchema(Path definitionPath) throws IOException {
+        List<String> definitionLines = Files.readAllLines(definitionPath);
+
+        // Set up a YAML ObjectMapper that collects the location of JsonNode's as they're parsed
+        ParserCapturingJsonFactory jsonFactory = new ParserCapturingJsonFactory(new YAMLFactory());
+        LocationTrackingNodeFactory nodeFactory =
+                new LocationTrackingNodeFactory(JsonNodeFactory.instance, jsonFactory::getParser);
+        ObjectMapper objectMapper = new ObjectMapper(jsonFactory).setNodeFactory(nodeFactory);
+
+        // Parse the file using JsonPath, allowing us to find a JsonNode by its XPath
+        DocumentContext ctx = JsonPath.parse(
+                definitionPath.toFile(),
+                Configuration.builder()
+                        .jsonProvider(new JacksonJsonNodeJsonProvider(objectMapper))
+                        .build());
+
+        Set<ValidationMessage> messages = schema.validate(ctx.json());
+
+        if (messages.isEmpty()) {
+            // If the JSON schema passes validation, then ensure there are no failure comments in the file
+            assertThat(definitionLines).noneMatch(failurePattern.asMatchPredicate());
+            return;
+        }
+
+        // Otherwise, for each validation message check the failure comment contains the correct error codes
+        messages.forEach(message -> {
+            ValidatorTypeCode code = lookupTypeCode(message.getCode());
+            JsonLocation location = nodeFactory.getLocationForNode(ctx.read(message.getPath()));
+
+            // We should find an error comment on the line preceding the failure
+            String precedingLine = definitionLines.get(location.getLineNr() - 2).trim();
+            Matcher matcher = failurePattern.matcher(precedingLine);
+            if (!matcher.matches()) {
+                throw new AssertionError(
+                        String.format("Unexpected JSON schema error '%s' %s", code, message.getMessage()));
+            }
+
+            // Extract the error codes from the comment and check against the message's error code
+            Set<String> errorCodes = Splitter.on(',').splitToList(matcher.group(1)).stream()
+                    .map(String::trim)
+                    .collect(Collectors.toSet());
+            assertThat(errorCodes)
+                    .as("Checking schema error ValidatorTypeCode '%s'", code)
+                    .contains(code.getValue());
+        });
+    }
+
+    private static ValidatorTypeCode lookupTypeCode(String messageCode) {
+        return Arrays.stream(ValidatorTypeCode.values())
+                .filter(validatorTypeCode -> validatorTypeCode.getErrorCode().equals(messageCode))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Unrecognized error code: " + messageCode));
+    }
+
+    private static Set<Path> definitionFiles() {
+        try (Stream<Path> paths = Files.walk(Paths.get("src/test/resources"))) {
+            return paths.filter(path -> path.toFile().isFile()).collect(Collectors.toSet());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ conjure/src/test/java/com/palantir/conjure/cli/ParserCapturingJsonFactory.java
@@ -0,0 +1,101 @@
+/*
+ * (c) Copyright 2022 Palantir Technologies Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.conjure.cli;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonParser;
+import java.io.DataInput;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.net.URL;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * A JsonFactory that captures the parser when it's created, making it available to use elsewhere.
+ */
+public final class ParserCapturingJsonFactory extends JsonFactory {
+
+    private final JsonFactory delegate;
+    private final AtomicReference<JsonParser> parserReference = new AtomicReference<>();
+
+    public ParserCapturingJsonFactory(JsonFactory delegate) {
+        this.delegate = delegate;
+    }
+
+    public JsonParser getParser() {
+        return Optional.ofNullable(parserReference.get()).orElseThrow();
+    }
+
+    @Override
+    public JsonParser createParser(File file) throws IOException, JsonParseException {
+        return captureParser(delegate.createParser(file));
+    }
+
+    @Override
+    public JsonParser createParser(URL url) throws IOException, JsonParseException {
+        return captureParser(delegate.createParser(url));
+    }
+
+    @Override
+    public JsonParser createParser(InputStream in) throws IOException, JsonParseException {
+        return captureParser(delegate.createParser(in));
+    }
+
+    @Override
+    public JsonParser createParser(Reader reader) throws IOException, JsonParseException {
+        return captureParser(delegate.createParser(reader));
+    }
+
+    @Override
+    public JsonParser createParser(byte[] data) throws IOException, JsonParseException {
+        return captureParser(delegate.createParser(data));
+    }
+
+    @Override
+    public JsonParser createParser(byte[] data, int offset, int len) throws IOException, JsonParseException {
+        return captureParser(delegate.createParser(data, offset, len));
+    }
+
+    @Override
+    public JsonParser createParser(String content) throws IOException, JsonParseException {
+        return captureParser(delegate.createParser(content));
+    }
+
+    @Override
+    public JsonParser createParser(char[] content) throws IOException {
+        return captureParser(delegate.createParser(content));
+    }
+
+    @Override
+    public JsonParser createParser(char[] content, int offset, int len) throws IOException {
+        return captureParser(delegate.createParser(content, offset, len));
+    }
+
+    @Override
+    public JsonParser createParser(DataInput in) throws IOException {
+        return captureParser(delegate.createParser(in));
+    }
+
+    private JsonParser captureParser(JsonParser parser) {
+        this.parserReference.set(parser);
+        return parser;
+    }
+}
